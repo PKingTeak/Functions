e05a5c3e9fd952797854dbdc8f00231a6743ab4a
PKT <rudxorrmrwkd@gmail.com>
2025-10-08 19:44:19 +0900

    Astar 이동은 됨 근데 장애물 판정이 이상함

---
+++ FunctionProject/Assets/Astar/ClickToMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour
+{
+    public GridGraph gridGraph;
+    public PathAgent pathAgent;
+    public KeyCode clickKey = KeyCode.Mouse0;
+
+    bool isMoving = false;
+
+
+    void Reset()
+    {
+        pathAgent = GetComponent<PathAgent>();
+    }
+
+
+    private void Update()
+    {
+        
+        
+
+
+        if (Input.GetKeyDown(clickKey) && Camera.main != null)
+        {
+            var mouseID = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseID.z = 0f; //2D이니까 
+
+
+            Vector2Int goal = gridGraph.WorldToCell(mouseID);
+            if(!gridGraph.InBounds(goal)) return;
+
+
+            Vector2Int start = gridGraph.WorldToCell(transform.position);
+
+            Debug.Log($"start:{start} , goal:{goal}");
+            if (!isMoving)
+            {
+                StartCoroutine(MoveRoutine(start, goal));
+            }
+
+
+        }
+
+    }
+
+
+    IEnumerator MoveRoutine(Vector2Int start, Vector2Int goal)
+    {
+        isMoving = true;
+        yield return pathAgent.Go(start, goal);
+        isMoving = false;
+        
+    }
+}
--- FunctionProject/Assets/Astar/GridGraph.cs
+++ FunctionProject/Assets/Astar/GridGraph.cs
@@ -19,7 +19,7 @@ public class GridGraph : MonoBehaviour
 
     private void Awake()
     {
-
+        Build();
         
     }
 
